8bb6e5f038162dc24d78f4ecb80421c2b62b1f34
Lucas Alysson <lalysson76@gmail.com>
2018-04-29 20:06:00 -0300

    finalizãção da parte de DAO

--- class/Usuario.php
+++ class/Usuario.php
@@ -2,11 +2,13 @@
 
 class Usuario{
 
+	//Atributos d classe
 	private $idusuario;
 	private $desloguin;
 	private $dessenha;
 	private $dtcadastro;
 
+	//GET's e SET's dos atributos
 	public function getIdusuario(){
 		return $this->idusuario;
 	}
@@ -24,7 +26,7 @@ class Usuario{
 	public function getDessenha(){
 		return $this->dessenha;
 	}
-	public function setIDessenha($value){
+	public function setDessenha($value){
 		$this->dessenha = $value;
 	}
 
@@ -35,7 +37,7 @@ class Usuario{
 		$this->dtcadastro = $value;
 	}
 
-	public function loadById($id){
+	public function loadById($id){//seleciona o usuário através do seu ID
 		$sql = new Sql();//carrega a classe SQL
 
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
@@ -43,13 +45,91 @@ class Usuario{
 		));//instancia o metodo select passado 3 parametros, são eles: $rawQuery, $params
 
 		if (count($results)>0) {
-			$row = $results[0];
-
-			$this->setIdusuario($row['idusuario']);
-			$this->setDesloguin($row['desloguin']);
-			$this->setIDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setdata($results[0]);
 		}
+	}
+
+	public static function getList(){//mostra uma lista de toos os usuários da tablea, ordenando pelo seu ID
+		$sql = new Sql();
+
+		return  $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario ");
+	}
+
+	public static function buscarUsuario($loguin){//mostra o usuário a partir da senha
+		$sql = new Sql();
+
+		return  $sql->select("SELECT * FROM tb_usuarios WHERE desloguin LIKE :BUSCA ORDER BY idusuario ", array(
+				":BUSCA" => "%".$loguin."%"
+		));
+	}
+
+	public function loguin($loguin, $password){//mostra o usuário a partir do loguin e senha
+		$sql = new Sql();//carrega a classe SQL
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE desloguin = :LOGUIN AND dessenha = :SENHA", array(
+			":LOGUIN" => $loguin,
+			":SENHA" => $password
+		));//instancia o metodo select passado 3 parametros, são eles: $rawQuery, $params
+
+		if (count($results)>0) {
+			$this->setdata($results[0]);
+
+			
+		}else{
+			throw new Exception("Loguin ou Senha inválidos");
+			
+		}
+	}
+
+	public function setData($data){//metodo geral para não repetir
+		$this->setIdusuario($data['idusuario']);
+		$this->setDesloguin($data['desloguin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new DateTime($data['dtcadastro']));
+	}
+
+	public function insert(){//iserir um novo usuário e mostra
+		$sql = new Sql();//carrega a classe SQL
+
+		$results = $sql->select("CALL sp_usuarios_insert(:LOGUIN,:SENHA)", array(
+			":LOGUIN" => $this->getDesloguin(),
+			":SENHA" => $this->getDessenha()
+		));
+		if (count($results)>0) {
+			$this->setdata($results[0]);
+
+			
+		}
+	}
+
+	public function __construct($login = "", $senha = ""){//consulta usuário através de loguin e senha
+		$this -> setDesloguin($login);
+		$this -> setDessenha($senha);
+	}
+
+	public function update($login,$senha){//alterar o usuário
+
+		$this->setDesloguin($login);
+		$this->setDessenha($senha);
+
+		$sql = new Sql();//intancia a classe SQL
+
+		$results = $sql->query("UPDATE tb_usuarios SET desloguin = :LOGUIN, dessenha = :SENHA WHERE idusuario = :ID", array(
+			":LOGUIN" => $this->getDesloguin(),
+			":SENHA" => $this->getDessenha(),
+			":ID" => $this->getIdusuario()
+		));
+		
+	}
+
+	public function delete(){//Apaga um usuário a partir do seu ID
+
+		$sql = new Sql();//intancia a classe SQL
+
+		$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID ", array(':ID' =>  $this->getIdusuario()
+		));
+
+		$this->setData(0);
 	}
 
 	public function __toString(){//realiza apresentação da array solicitado do ID passado no Index para uma apresentação direta no echo
--- index.php
+++ index.php
@@ -2,16 +2,45 @@
 
 require_once("config.php");
 
+//Mostrar toda a tabela
 /*$sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
-
+//Carrega usuario
+/*$root = new Usuario();
 $root -> loadById(19);
+echo $root;*/
 
-echo $root;
+/*CARREGAR UMA LISTA DO USUÁRIO
+$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//cARREGA UMA LISTA PARA O USUÁRIO ATRAVÉS DO LOGIN
+/*$searche = Usuario::buscarUsuario('jo');
+echo json_encode($searche);*/
+
+//Carrega usuario através de usuario e senha
+/*$usuario = new Usuario();
+$usuario -> loguin("root", "!@#$0");
+echo $usuario;*/
+
+/* Criando um novo usário
+$aluno =  new Usuario("LOUCO","CHUVA");
+$aluno -> insert();
+echo $aluno;*/
+
+/*Altera um usuário
+$usuario =  new Usuario();
+$usuario -> loadById(30);
+$usuario -> update("professor","ksaksk");
+echo $usuario;*/
+
+$eliminar = new Usuario();
+$eliminar->loadById(27);
+$eliminar->delete();
+
+echo $eliminar;
+
 
 ?>
